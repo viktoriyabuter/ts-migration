6a94fdef4a94e06bde1cfdbb236963d2298042fb
viktoriyabuter <viktoriya.buter@gmail.com>
2026-03-19 17:53:05 +0300

    add deliveryCost.ts

---
+++ deliveryCost.ts
@@ -0,0 +1,46 @@
+type PackageSize = 'small' | 'large';
+type Workload = 'very high' | 'high' | 'increased' | 'normal';
+
+function calculateDeliveryCost(
+    distance: number,
+    size: PackageSize,
+    fragile: boolean,
+    workload: Workload
+): number {
+    const minCost = 400;
+    let cost = 0;
+
+    if (fragile && distance > 30) {
+        throw new Error("Fragile packages cannot be delivered over distances greater than 30 km");
+    }
+
+    if (distance > 30) {
+        cost += 300;
+    } else if (distance > 10) {
+        cost += 200;
+    } else if (distance > 2) {
+        cost += 100;
+    } else {
+        cost += 50;
+    }
+
+    cost += (size ==='large') ? 200 : 100
+
+    if (fragile) cost += 300;
+
+    const workloadCoefficients : Record<Workload, number> = {
+        'very high': 1.6,
+        'high': 1.4,
+        'increased': 1.2,
+        'normal': 1.0
+    }
+
+    cost *= workloadCoefficients[workload];
+
+    return Math.max(cost, minCost);
+}
+
+console.log(calculateDeliveryCost(5, 'small', false, 'normal'));
+console.log(calculateDeliveryCost(1, 'large', true, 'high'));
+console.log(calculateDeliveryCost(25, 'small', true, 'very high'));
+console.log(calculateDeliveryCost(60, 'small', true, 'high'));
