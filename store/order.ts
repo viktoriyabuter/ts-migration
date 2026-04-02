a7457e695ea669224434cea3719037ed5bbf0a62
viktoriyabuter <viktoriya.buter@gmail.com>
2026-04-02 18:49:59 +0300

    structure reformat, added order interface, order class

---
+++ store/order.ts
@@ -0,0 +1,38 @@
+//Create an interface and the class for the Pet and Order DTO from Swagger
+//Petstore: https://petstore.swagger.io/#/
+
+enum OrderStatus {
+  Placed = "PLACED",
+  Approved = "APPROVED",
+  Delivered = "DELIVERED",
+}
+
+interface Order {
+  id?: number;
+  petId?: number;
+  quantity?: number;
+  shipDate?: string;
+  status?: OrderStatus;
+  complete?: boolean;
+}
+
+class OrderDTO implements Order {
+  id?: number;
+  petId?: number;
+  quantity?: number;
+  shipDate?: string;
+  status?: OrderStatus;
+  complete?: boolean;
+
+  constructor(data?: Partial<Order>) {
+    Object.assign(this, data);
+  }
+}
+
+const order1 = new OrderDTO();
+const order2 = new OrderDTO({
+  id: 1,
+  shipDate: "2026-04-02T15:30:00Z",
+});
+console.log(order1.id);
+console.log(order2.shipDate);
